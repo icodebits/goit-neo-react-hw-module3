4f12eab20f955f52efc753288b2762fef51a547d
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-08-29 18:32:06 +0300

    Implement Step 1.

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,13 @@
+import Contact from '../Contact/Contact';
+import styles from './ContactList.module.css';
+const ContactList = ({ contacts }) => {
+  return (
+    <ul className={styles.contactList}>
+      {contacts.map(contact => (
+        <Contact key={contact.id} name={contact.name} number={contact.number} />
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
