72b7aa6f30edc9dfc02dbc5de7a1114bdf7cac1a
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-08-29 19:30:44 +0300

    Implement Step 3.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import ContactList from '../ContactList/ContactList';
 import SearchBox from '../SearchBox/SearchBox';
+import ContactForm from "../ContactForm/ContactForm";
 
 
 function App() {
-  const [contacts] = useState([
+  const [contacts, setContacts] = useState([
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
@@ -18,15 +19,20 @@ function App() {
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
+  const addContact = (newContact) => {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
+
   return (
     <>
       <div>
         <h1>Phonebook</h1>
+        <ContactForm onSubmit={addContact} />
         <SearchBox value={filter} onChange={setFilter} />
         <ContactList contacts={filteredContacts} />
       </div>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,72 @@
+import { Formik, Form, Field, ErrorMessage } from "formik"
+import * as Yup from "yup"
+import { nanoid } from "nanoid"
+import styles from "./ContactForm.module.css"
+
+const ContactForm = ({ onSubmit }) => {
+    const validationSchema = Yup.object({
+        name: Yup.string()
+            .min(3, "Must be at least 3 characters")
+            .max(50, "Must be 50 characters or less")
+            .required("Required"),
+        number: Yup.string()
+            .min(3, "Must be at least 3 characters")
+            .max(50, "Must be 50 characters or less")
+            .required("Required"),
+    })
+
+    const initialValues = {
+        name: "",
+        number: "",
+    }
+
+    const handleSubmit = (values, { resetForm }) => {
+        const newContact = {
+            id: nanoid(),
+            name: values.name,
+            number: values.number,
+        }
+        onSubmit(newContact)
+        resetForm()
+    }
+
+    return (
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting, isValid }) => (
+          <Form className={styles.contactForm}>
+            <div className={styles.formGroup}>
+              <label htmlFor="name">Name</label>
+              <Field type="text" name="name" className={styles.inputField} />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className={styles.errorMessage}
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="number">Number</label>
+              <Field type="text" name="number" className={styles.inputField} />
+              <ErrorMessage
+                name="number"
+                component="div"
+                className={styles.errorMessage}
+              />
+            </div>
+            <button
+              type="submit"
+              className={styles.submitButton}
+              disabled={!isValid || isSubmitting}
+            >
+              Add Contact
+            </button>
+          </Form>
+        )}
+      </Formik>
+    );
+}
+
+export default ContactForm
