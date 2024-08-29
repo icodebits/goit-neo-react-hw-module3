3a7edf342d201e084cba64f0d7aa7396eba00e76
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-08-29 20:21:45 +0300

    Implement Step 5.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 import './App.css'
 import ContactList from '../ContactList/ContactList'
 import SearchBox from '../SearchBox/SearchBox'
 import ContactForm from "../ContactForm/ContactForm"
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ])
+  const defaultContacts = [
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ]
 
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts")
+    const parsedContacts = JSON.parse(savedContacts)
+
+    if (parsedContacts !== null && parsedContacts.length > 0) {
+      return parsedContacts
+    } else {
+      return defaultContacts
+    }
+  })
+  
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts])
 
   const addContact = (newContact) => {
     setContacts((prevContacts) => [...prevContacts, newContact])
